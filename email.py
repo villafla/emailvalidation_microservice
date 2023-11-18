e9b1a4a75352bb1b9da63f426924d604d2e5a0c9
Allyson Villaflor <132638899+villafla@users.noreply.github.com>
2023-11-17 19:29:13 -0800

    Update email.py
    Updated microservice using Flask. Validates emails by sending POST requests to the microservice's endpoint.

--- email.py
+++ email.py
@@ -1,12 +1,25 @@
-import re  
-  
-def validate_email(email):  
-    if re.match(r"[^@]+@[^@]+\.[^@]+", email):  
-        return True  
-    return False  
-  
-email = "example@domain.com"  
-if validate_email(email):  
-    print("Valid email address")  
-else:  
-    print("Invalid email address")  
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/validate-email', methods=['POST'])
+def validate_email():
+    data = request.get_json()
+    email = data.get('email')
+
+    is_valid = validate_email_function(email)
+
+    response_data = {
+        'email': email,
+        'is_valid': is_valid
+    }
+
+    return jsonify(response_data)
+
+def validate_email_function(email):
+    import re
+    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    return bool(re.match(pattern, email))
+
+if __name__ == '__main__':
+    app.run(debug=True)
